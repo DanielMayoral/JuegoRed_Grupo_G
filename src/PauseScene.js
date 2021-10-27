0c904f6d0d044c828c19fe53ba402a28e151a4d4
DanielMayoral <91623804+DanielMayoral@users.noreply.github.com>
2021-10-27 21:03:36 +0200

    Actualización PauseScene
    Arreglo errores menores

--- src/PauseScene.js
+++ src/PauseScene.js
@@ -16,28 +16,27 @@ export default class PauseScene extends Phaser.Scene{
 
         this.cameras.main.setViewport(widthGame/2-this.width/2, heightGame/2-this.height/2, this.width, this.height);
 
+        this.input.gamepad.start();
+
         this.playScene = this.scene.get('PlayScene');
 
-        var padP1;
-        var padP2;
+        var padP1 = this.registry.get('padP1');
+        var padP2 = this.registry.get('padP2');
 
+        
         //recives the index of the gamepad chosen in PlayScene
         //and reassigns the gamepad with the same index
-        this.playScene.events.on('indexP1',function(data){
-            for(var i=0;i<this.input.gamepad.gamepads.length;i++){
-                if(data.indexP1 === this.input.gamepad.gamepads[i].index){
-                    padP1 = this.input.gamepad.gamepads[i];
-                }
+        for(var i=0;i<this.input.gamepad.gamepads.length;i++){
+            if(padP1.index === this.input.gamepad.gamepads[i].index){
+                padP1 = this.input.gamepad.gamepads[i];
             }
-        })
+        }
 
-        this.playScene.events.on('indexP2',function(data){
-            for(var i=0;i<this.input.gamepad.gamepads.length;i++){
-                if(data.indexP2 === this.input.gamepad.gamepads[i].index){
-                    padP2 = this.input.gamepad.gamepads[i];
-                }
+        for(var i=0;i<this.input.gamepad.gamepads.length;i++){
+            if(padP2.index === this.input.gamepad.gamepads[i].index){
+                padP2 = this.input.gamepad.gamepads[i];
             }
-        })
+        }
 
         this.pauseText = this.make.text({
             x: this.width/2,
@@ -49,6 +48,8 @@ export default class PauseScene extends Phaser.Scene{
             }
         });
         this.pauseText.setOrigin(0.5,0.5);
+
+        
         
         //resume PlayScene if player push up the start button of the gamepad
         padP1.on('up',(index,value,button) =>{
