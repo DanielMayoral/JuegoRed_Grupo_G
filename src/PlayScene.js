d7a996a73f247ab227b5dee5e018552e5f24254b
DanielMayoral <91623804+DanielMayoral@users.noreply.github.com>
2021-10-27 18:32:10 +0200

    Actualización PlayScene
    Se ha añadido mejoras a generación de objetos para tener en cuenta los nuevos assets, pausa del juego mediante el mando y mejoras en la asignación de los mandos.

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,6 +1,6 @@
 import Player from "./player.js";
+import PauseScene from "./PauseScene.js";
 import {widthGame, heightGame} from './main.js';
-//import Objects from "./objects.js";
 
 export default class PlayScene extends Phaser.Scene{
     constructor(){
@@ -9,13 +9,20 @@ export default class PlayScene extends Phaser.Scene{
     }
 
     preload(){
+        //load the cursor
         this.load.spritesheet('cursor','assets/cursorprueba1.png', {frameWidth:60, frameHeight: 78});
+
+        //load the background image
         this.load.image('fondojuego','assets/fondopartida.png');
+
+        //load the assets for the objects
         this.load.image('cake1','assets/pasteles/PastelMorado.png');
         this.load.image('cake2','assets/pasteles/PastelVerde.png');
         this.load.image('cake3','assets/pasteles/PastelRosa.png');
         this.load.image('cookie','/assets/Galletas.png')
-        this.load.image('coffee','assets/Cafe.png');
+        this.load.image('coffee1','assets/cafes/Cafe1.png');
+        this.load.image('coffee2','assets/cafes/Cafe2.png');
+        this.load.image('coffee3','assets/cafes/Cafe3.png');
     }
 
     create(){
@@ -30,7 +37,7 @@ export default class PlayScene extends Phaser.Scene{
         //time variable to generate items
         this.generateTime = 4000;
         this.cookieTime = Phaser.Math.Between(8000,12000);
-        this.coffeTime = Phaser.Math.Between(25000,35000);
+        this.cakeTime = Phaser.Math.Between(25000,35000);
 
         //time variable for play time control
         this.initalTime = 120;
@@ -70,19 +77,36 @@ export default class PlayScene extends Phaser.Scene{
 
 
         //generate cakes event
-        this.timeGenerate = this.time.addEvent({delay: this.generateTime, callback: this.createCake, callbackScope:this, loop:true});
+        this.generateCoffee = this.time.addEvent({delay: this.generateTime, callback: this.createCoffee, callbackScope:this, loop:true});
         this.generateCookie = this.time.addEvent({delay:this.cookieTime, callback:this.createCookie, callbackScope:this, loop:true});
-        this.generateCoffee = this.time.addEvent({delay:this.coffeTime, callback:this.createCoffee, callbackScope:this, loop:true});
+        this.generateCake = this.time.addEvent({delay:this.cakeTime, callback:this.createCake, callbackScope:this, loop:true});
         //game time event
         this.texTimer = this.add.text(widthGame/2-32,0,this.formatedTime(this.initalTime));
         this.timedEvent = this.time.addEvent({delay: 1000, callback: this.onEvent, callbackScope:this, loop:true});
     
         this.cursors = this.input.keyboard.createCursorKeys();
         this.actionJ1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO);
+    
+    
+        //pause the scene with start button
+        this.input.gamepad.pad1.on('up',(index,value,button) =>{
+            if(index === 9){
+                this.createWindow();
+                this.scene.pause();
+            }
+        });
+
+        this.input.gamepad.pad3.on('up',(index,value,button) =>{
+            if(index === 9){
+                this.createWindow();
+                this.scene.pause();
+            }
+        });
     }
 
     update(){
 
+        //reasing delay time
         this.timeGenerate.delay = this.generateTime;
 
         if(this.initalTime <= 0){
@@ -104,28 +128,78 @@ export default class PlayScene extends Phaser.Scene{
                 //tilt, and if it isn't, it is changed to the other one
                 this.gamepadJ1 = this.input.gamepad.pad1;
                 if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                    this.gamepadJ1 = this.input.gamepad.pad3;
+                    this.gamepadJ1 = this.input.gamepad.pad2;
+                    if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadJ1 = this.input.gamepad.pad3;
+                        if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                            this.gamepadJ1 = this.input.gamepad.pad4;
+                            this.mandoElegido = 4;
+                        }else{
+                            this.mandoElegido = 3;
+                        }
+                    }else{
+                        this.mandoElegido = 2;
+                    }
+                }else{
+                    this.mandoElegido = 1;
                 }
                 
+                //comprobar todos los pad para primer jugador, variable control que pad asignado
+                //comprobar todos los pad menos el asignado para segundo jugador
+                //move the pointer with the axis movement
                 if(this.gamepadJ1.axes.length){
                     var axisHJ1 = this.gamepadJ1.axes[0].getValue();
                     var axisVJ1 = this.gamepadJ1.axes[1].getValue();
 
-                    this.player1.x += 10 * axisHJ1;
-                    this.player1.y += 10 * axisVJ1;
+                    this.player1.x += 8 * axisHJ1;
+                    this.player1.y += 8 * axisVJ1;
                 }
 
-                this.gamepadJ2 = this.input.gamepad.pad2;
-                if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                    this.gamepadJ2 = this.input.gamepad.pad4;
+                switch(this.mandoElegido){
+                    case 1:
+                        this.gamepadJ2 = this.input.gamepad.pad2;
+                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                            this.gamepadJ2 = this.input.gamepad.pad3;
+                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                                this.gamepadJ2 = this.input.gamepad.pad4;
+                            }
+                        }
+                        break;
+                    case 2:
+                        this.gamepadJ2 = this.input.gamepad.pad1;
+                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                            this.gamepadJ2 = this.input.gamepad.pad3;
+                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                                this.gamepadJ2 = this.input.gamepad.pad4;
+                            }
+                        }
+                        break;
+                    case 3:
+                        this.gamepadJ2 = this.input.gamepad.pad1;
+                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                            this.gamepadJ2 = this.input.gamepad.pad2;
+                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                                this.gamepadJ2 = this.input.gamepad.pad4;
+                            }
+                        }
+                        break;
+                    case 4:
+                        this.gamepadJ2 = this.input.gamepad.pad1;
+                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                            this.gamepadJ2 = this.input.gamepad.pad2;
+                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                                this.gamepadJ2 = this.input.gamepad.pad3;
+                            }
+                        }
+                        break;
                 }
                 
                 if(this.gamepadJ2.axes.length){
                     var axisHJ2 = this.gamepadJ2.axes[0].getValue();
                     var axisVJ2 = this.gamepadJ2.axes[1].getValue();
 
-                    this.player2.x += 10 * axisHJ2;
-                    this.player2.y += 10 * axisVJ2;
+                    this.player2.x += 8 * axisHJ2;
+                    this.player2.y += 8 * axisVJ2;
                 }
                 
             }
@@ -149,12 +223,12 @@ export default class PlayScene extends Phaser.Scene{
     collectCakeJ1(player1,cake){
         if(this.input.gamepad.total !== 0){
             if(this.gamepadJ1.A){
-                if(cake.texture.key === 'cake'){
-                    player1.addPoint(1);
+                if(cake.texture.key === 'cake1' || cake.texture.key === 'cake2' || cake.texture.key === 'cake3'){
+                    player1.addPoint(250);
                 }else if(cake.texture.key === 'cookie'){
-                    player1.addPoint(2);
+                    player1.addPoint(150);
                 }else{
-                    player1.addPoint(3);
+                    player1.addPoint(50);
                 }
                 cake.destroy();
                 //console.log("Pastel cogido J1");
@@ -165,7 +239,7 @@ export default class PlayScene extends Phaser.Scene{
     collectCakeJ2(player2,cake){
         if(this.input.gamepad.total !== 0){
             if(this.gamepadJ2.A){
-                if(cake.texture.key === 'cake'){
+                if(cake.texture.key === 'cake1' || cake.texture.key === 'cake2' || cake.texture.key === 'cake3'){
                     player2.addPoint(250);
                 }else if(cake.texture.key === 'cookie'){
                     player2.addPoint(150);
@@ -178,35 +252,40 @@ export default class PlayScene extends Phaser.Scene{
         }
     }
 
+
+    //function to create cake object
     createCake(){
         var posX = Phaser.Math.Between(50, widthGame-100);
         var cake;
         var angle = -1.57;
         var numAle = Phaser.Math.Between(1, 3);
-        cake = this.cakes.create(posX,heightGame+50,'cake'+numAle);
-        this.generateTime -= 100;
-        this.physics.velocityFromRotation(angle,375,cake.body.velocity);
-        if(this.generateTime <= 1000){
-            this.generateTime = 1000;
-        }
+        cake = this.cakes.create(posX,heightGame+50,'cake'+numAle).setScale(0.5);
+        this.physics.velocityFromRotation(angle,375,cake.body.velocity);       
         cake.setGravity(0,200);
     }
 
+    //function to create cookie object
     createCookie(){
         var posX = Phaser.Math.Between(50, widthGame-100);
         var cookie;
         var angle = -1.57;
-        cookie = this.cakes.create(posX,heightGame+50,'cookie').setScale(0.2);
+        cookie = this.cakes.create(posX,heightGame+50,'cookie').setScale(0.5);
         this.physics.velocityFromRotation(angle,375,cookie.body.velocity);
         cookie.setGravity(0,250);
     }
 
+    //function to create coffee object
     createCoffee(){
         var posX = Phaser.Math.Between(50, widthGame-100);
         var coffee;
         var angle = -1.57;
-        coffee = this.cakes.create(posX,heightGame+50,'coffee').setScale(0.2);
+        var numAle = Phaser.Math.Between(1,3);
+        coffee = this.cakes.create(posX,heightGame+50,'coffee'+numAle).setScale(0.5);
         this.physics.velocityFromRotation(angle,375,coffee.body.velocity);
+        this.generateTime -= 100;
+        if(this.generateTime <= 1000){
+            this.generateTime = 1000;
+        }
         coffee.setGravity(0,300);
     }
 
@@ -223,5 +302,14 @@ export default class PlayScene extends Phaser.Scene{
         this.initalTime -= 1;
         this.texTimer.setText(this.formatedTime(this.initalTime));
         
+    }
+
+    //function to create a PauseScene
+    createWindow(){
+        var win = this.add.zone(widthGame/2,heightGame/2,500,200).setInteractive().setOrigin(0);
+
+        var demo = new PauseScene();
+
+        this.scene.add('PauseScene',demo,true);
     }
 }
