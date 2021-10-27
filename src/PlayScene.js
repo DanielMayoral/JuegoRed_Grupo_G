42b2d0c7d4b15275d0f50fc8141fee57e9d23cfb
DanielMayoral <91623804+DanielMayoral@users.noreply.github.com>
2021-10-27 19:38:03 +0200

    Actualización PlayScene
    Mejora en la lógica de selección de mandos y paso de información del index de los mandos a PauseScene

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -12,6 +12,7 @@ export default class PlayScene extends Phaser.Scene{
         //load the cursor
         this.load.spritesheet('cursorjug1','assets/cursores/cursormorado.png', {frameWidth:60, frameHeight: 78});
         this.load.spritesheet('cursorjug2','assets/cursores/cursorverde.png', {frameWidth:60, frameHeight: 78});
+
         //load the background image
         this.load.image('fondojuego','assets/fondopartida.png');
 
@@ -23,17 +24,21 @@ export default class PlayScene extends Phaser.Scene{
         this.load.image('coffee1','assets/cafes/Cafe1.png');
         this.load.image('coffee2','assets/cafes/Cafe2.png');
         this.load.image('coffee3','assets/cafes/Cafe3.png');
+
+        //load the audio
         this.load.audio('mgame', 'music/A_Hoot.wav');
     }
 
     create(){
         this.add.image(0,0,'fondojuego').setOrigin(0,0).setScale(0.5);
+
         var musicgame = this.sound.add('mgame', {volume: 0.3});
         musicgame.setLoop(true);
         musicgame.play();
+
         //create player and objects
-        this.player1 = new Player(this,100,heightGame-20,'cursorjug1');
-        this.player2 = new Player(this,200,heightGame-20,'cursorjug2');
+        this.player1 = new Player(this,100,heightGame-20,'cursor');
+        this.player2 = new Player(this,200,heightGame-20,'cursor');
 
         this.cakes = this.physics.add.group();
         //this.cake = this.cakes.create(100,100, 'cake').setScale(0.25);
@@ -87,20 +92,77 @@ export default class PlayScene extends Phaser.Scene{
         //game time event
         this.texTimer = this.add.text(widthGame/2-32,0,this.formatedTime(this.initalTime));
         this.timedEvent = this.time.addEvent({delay: 1000, callback: this.onEvent, callbackScope:this, loop:true});
+
+        //The computer allocates two pads to the same physical controller,
+        //one with tilt and the other without it. The tilt is necessary to play 
+        //and that's why it should be check if the gamepad's game is the pad with 
+        //tilt, and if it isn't, it is changed to the other one
+        this.gamepadP1 = this.input.gamepad.pad1;
+            if(this.gamepadP1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                this.gamepadP1 = this.input.gamepad.pad2;
+                if(this.gamepadP1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                    this.gamepadP1 = this.input.gamepad.pad3;
+                    if(this.gamepadP1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadP1 = this.input.gamepad.pad4;
+                        this.padSelect = 4;
+                    }else{
+                        this.padSelect = 3;
+                    }
+                }else{
+                    this.padSelect = 2;
+                }
+            }else{
+                this.padSelect = 1;
+            }
     
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.actionJ1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO);
-    
-    
+        switch(this.padSelect){
+            case 1:
+                this.gamepadP2 = this.input.gamepad.pad2;
+                if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                    this.gamepadP2 = this.input.gamepad.pad3;
+                    if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadP2 = this.input.gamepad.pad4;
+                    }
+                }
+                break;
+            case 2:
+                this.gamepadP2 = this.input.gamepad.pad1;
+                if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                    this.gamepadP2 = this.input.gamepad.pad3;
+                    if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadP2 = this.input.gamepad.pad4;
+                    }
+                }
+                break;
+            case 3:
+                this.gamepadP2 = this.input.gamepad.pad1;
+                if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                    this.gamepadP2 = this.input.gamepad.pad2;
+                    if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadP2 = this.input.gamepad.pad4;
+                    }
+                }
+                break;
+            case 4:
+                this.gamepadP2 = this.input.gamepad.pad1;
+                if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                    this.gamepadP2 = this.input.gamepad.pad2;
+                    if(this.gamepadP2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
+                        this.gamepadP2 = this.input.gamepad.pad3;
+                    }
+                }
+            break;
+        }
+
         //pause the scene with start button
-        this.input.gamepad.pad1.on('up',(index,value,button) =>{
+        this.gamepadP1.on('up',(index,value,button) =>{
             if(index === 9){
                 this.createWindow();
                 this.scene.pause();
             }
         });
 
-        this.input.gamepad.pad3.on('up',(index,value,button) =>{
+        this.gamepadP2.on('up',(index,value,button) =>{
             if(index === 9){
                 this.createWindow();
                 this.scene.pause();
@@ -111,13 +173,15 @@ export default class PlayScene extends Phaser.Scene{
     update(){
 
         //reasing delay time
-        this.timeGenerate.delay = this.generateTime;
+        this.generateCoffee.delay = this.generateTime;
 
         if(this.initalTime <= 0){
             this.gameOver = true;
         }
 
         if(!this.gameOver){
+
+            //console.log(this.input.gamepad.pad2.index);
             //update points
             this.pointsTextP1.setText('Points Player 1: '+this.player1.getPoints());
             this.pointsTextP2.setText('Points Player 2: '+this.player2.getPoints());
@@ -125,90 +189,27 @@ export default class PlayScene extends Phaser.Scene{
             if(this.input.gamepad.total === 0){
                 //console.log("No hay gamepad");
             }else{
-
-                //The computer allocates two pads to the same physical controller,
-                //one with tilt and the other without it. The tilt is necessary to play 
-                //and that's why it should be check if the gamepad's game is the pad with 
-                //tilt, and if it isn't, it is changed to the other one
-                this.gamepadJ1 = this.input.gamepad.pad1;
-                if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                    this.gamepadJ1 = this.input.gamepad.pad2;
-                    if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                        this.gamepadJ1 = this.input.gamepad.pad3;
-                        if(this.gamepadJ1.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                            this.gamepadJ1 = this.input.gamepad.pad4;
-                            this.mandoElegido = 4;
-                        }else{
-                            this.mandoElegido = 3;
-                        }
-                    }else{
-                        this.mandoElegido = 2;
-                    }
-                }else{
-                    this.mandoElegido = 1;
-                }
-                
-                //comprobar todos los pad para primer jugador, variable control que pad asignado
-                //comprobar todos los pad menos el asignado para segundo jugador
                 //move the pointer with the axis movement
-                if(this.gamepadJ1.axes.length){
-                    var axisHJ1 = this.gamepadJ1.axes[0].getValue();
-                    var axisVJ1 = this.gamepadJ1.axes[1].getValue();
+                if(this.gamepadP1.axes.length){
+                    var axisHJ1 = this.gamepadP1.axes[0].getValue();
+                    var axisVJ1 = this.gamepadP1.axes[1].getValue();
 
                     this.player1.x += 8 * axisHJ1;
                     this.player1.y += 8 * axisVJ1;
                 }
 
-                switch(this.mandoElegido){
-                    case 1:
-                        this.gamepadJ2 = this.input.gamepad.pad2;
-                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                            this.gamepadJ2 = this.input.gamepad.pad3;
-                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                                this.gamepadJ2 = this.input.gamepad.pad4;
-                            }
-                        }
-                        break;
-                    case 2:
-                        this.gamepadJ2 = this.input.gamepad.pad1;
-                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                            this.gamepadJ2 = this.input.gamepad.pad3;
-                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                                this.gamepadJ2 = this.input.gamepad.pad4;
-                            }
-                        }
-                        break;
-                    case 3:
-                        this.gamepadJ2 = this.input.gamepad.pad1;
-                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                            this.gamepadJ2 = this.input.gamepad.pad2;
-                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                                this.gamepadJ2 = this.input.gamepad.pad4;
-                            }
-                        }
-                        break;
-                    case 4:
-                        this.gamepadJ2 = this.input.gamepad.pad1;
-                        if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                            this.gamepadJ2 = this.input.gamepad.pad2;
-                            if(this.gamepadJ2.id !== 'Xbox 360 Controller (XInput STANDARD GAMEPAD)'){
-                                this.gamepadJ2 = this.input.gamepad.pad3;
-                            }
-                        }
-                        break;
-                }
                 
-                if(this.gamepadJ2.axes.length){
-                    var axisHJ2 = this.gamepadJ2.axes[0].getValue();
-                    var axisVJ2 = this.gamepadJ2.axes[1].getValue();
+                
+                if(this.gamepadP2.axes.length){
+                    var axisHJ2 = this.gamepadP2.axes[0].getValue();
+                    var axisVJ2 = this.gamepadP2.axes[1].getValue();
 
                     this.player2.x += 8 * axisHJ2;
                     this.player2.y += 8 * axisVJ2;
-                }
-                
+                }  
             }
         }else{
-            var winnerText = this.make.text({
+            var gameOverText = this.make.text({
                 x:widthGame/2,
                 y:heightGame/2,
                 text:'Game Over',
@@ -217,7 +218,7 @@ export default class PlayScene extends Phaser.Scene{
                     fill:'#ffffff'
                 }
             });
-            winnerText.setOrigin(0.5,0.5).setDepth(0);
+            gameOverText.setOrigin(0.5,0.5).setDepth(0);
 
             this.scene.pause();
         }
@@ -226,7 +227,7 @@ export default class PlayScene extends Phaser.Scene{
     
     collectCakeJ1(player1,cake){
         if(this.input.gamepad.total !== 0){
-            if(this.gamepadJ1.A){
+            if(this.gamepadP1.A){
                 if(cake.texture.key === 'cake1' || cake.texture.key === 'cake2' || cake.texture.key === 'cake3'){
                     player1.addPoint(250);
                 }else if(cake.texture.key === 'cookie'){
@@ -242,7 +243,7 @@ export default class PlayScene extends Phaser.Scene{
 
     collectCakeJ2(player2,cake){
         if(this.input.gamepad.total !== 0){
-            if(this.gamepadJ2.A){
+            if(this.gamepadP2.A){
                 if(cake.texture.key === 'cake1' || cake.texture.key === 'cake2' || cake.texture.key === 'cake3'){
                     player2.addPoint(250);
                 }else if(cake.texture.key === 'cookie'){
@@ -262,8 +263,8 @@ export default class PlayScene extends Phaser.Scene{
         var posX = Phaser.Math.Between(50, widthGame-100);
         var cake;
         var angle = -1.57;
-        var numAle = Phaser.Math.Between(1, 3);
-        cake = this.cakes.create(posX,heightGame+50,'cake'+numAle).setScale(0.5);
+        var randNum = Phaser.Math.Between(1, 3);
+        cake = this.cakes.create(posX,heightGame+50,'cake'+randNum).setScale(0.5);
         this.physics.velocityFromRotation(angle,375,cake.body.velocity);       
         cake.setGravity(0,200);
     }
@@ -283,8 +284,8 @@ export default class PlayScene extends Phaser.Scene{
         var posX = Phaser.Math.Between(50, widthGame-100);
         var coffee;
         var angle = -1.57;
-        var numAle = Phaser.Math.Between(1,3);
-        coffee = this.cakes.create(posX,heightGame+50,'coffee'+numAle).setScale(0.5);
+        var randNum = Phaser.Math.Between(1,3);
+        coffee = this.cakes.create(posX,heightGame+50,'coffee'+randNum).setScale(0.5);
         this.physics.velocityFromRotation(angle,375,coffee.body.velocity);
         this.generateTime -= 100;
         if(this.generateTime <= 1000){
@@ -311,6 +312,9 @@ export default class PlayScene extends Phaser.Scene{
     //function to create a PauseScene
     createWindow(){
         var win = this.add.zone(widthGame/2,heightGame/2,500,200).setInteractive().setOrigin(0);
+
+        this.events.emit('indexP1', ({indexP1: this.gamepadP1.index}));
+        this.events.emit('indexP2', ({indexP2: this.gamepadP2.index}));
 
         var demo = new PauseScene();
 
