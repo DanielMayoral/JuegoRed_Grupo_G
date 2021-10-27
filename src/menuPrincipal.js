8e79bc030da802de2419488c8151cb6e6079e8bb
AlrekVikahrr <91626409+AlrekVikahrr@users.noreply.github.com>
2021-10-27 18:43:02 +0200

    Update menuPrincipal.js

--- src/menuPrincipal.js
+++ src/menuPrincipal.js
@@ -8,10 +8,13 @@ export default class menuPrincipal extends Phaser.Scene{
     preload(){
         this.load.image('fondo','assets/fondomenuprincipal.png')
         this.load.image('empezar','assets/botonEmpezar.png')
+        this.load.audio('mprincipal', 'music/CLASSICA.wav')
     }
 
     create(){
         this.add.image(0,0,'fondo').setScale(0.5).setOrigin(0,0);
+        var music = this.sound.add('mprincipal', {volume: 0.3});
+        music.play();
         var boton = this.add.image((widthGame/2)-100,(heightGame/4)*3,'empezar').setScale(0.5).setOrigin(0,0);
         boton.setInteractive();
         
