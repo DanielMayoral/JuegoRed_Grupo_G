343eb194dcf6f5431e330d405a4b4d3e12b594fd
DanielMayoral <91623804+DanielMayoral@users.noreply.github.com>
2021-10-27 19:32:05 +0200

    Actualización PauseScene
    Añadida una mejora en la lógica de mandos y de reinicio de la escena PlayScene

--- src/PauseScene.js
+++ src/PauseScene.js
@@ -4,8 +4,8 @@ export default class PauseScene extends Phaser.Scene{
     constructor(){
         super({key: 'PauseScene'});
 
-        this.ancho = 500;
-        this.alto = 300;
+        this.width = 500;
+        this.height = 300;
     }
 
     preload(){
@@ -14,31 +14,57 @@ export default class PauseScene extends Phaser.Scene{
 
     create(){
 
-        this.cameras.main.setViewport(widthGame/2-this.ancho/2, heightGame/2-this.alto/2, this.ancho, this.alto);
+        this.cameras.main.setViewport(widthGame/2-this.width/2, heightGame/2-this.height/2, this.width, this.height);
 
-        this.textoPause = this.make.text({
-            x: this.ancho/2,
-            y: this.alto/2-50,
+        this.playScene = this.scene.get('PlayScene');
+
+        var padP1;
+        var padP2;
+
+        //recives the index of the gamepad chosen in PlayScene
+        //and reassigns the gamepad with the same index
+        this.playScene.events.on('indexP1',function(data){
+            for(var i=0;i<this.input.gamepad.gamepads.length;i++){
+                if(data.indexP1 === this.input.gamepad.gamepads[i].index){
+                    padP1 = this.input.gamepad.gamepads[i];
+                }
+            }
+        })
+
+        this.playScene.events.on('indexP2',function(data){
+            for(var i=0;i<this.input.gamepad.gamepads.length;i++){
+                if(data.indexP2 === this.input.gamepad.gamepads[i].index){
+                    padP2 = this.input.gamepad.gamepads[i];
+                }
+            }
+        })
+
+        this.pauseText = this.make.text({
+            x: this.width/2,
+            y: this.height/2-50,
             text: 'Pause',
             style:{
                 font: '32px monospace',
                 fill: '#ffffff'
             }
         });
-        this.textoPause.setOrigin(0.5,0.5);
+        this.pauseText.setOrigin(0.5,0.5);
         
-        this.input.gamepad.pad1.on('up',(index,value,button) =>{
+        //resume PlayScene if player push up the start button of the gamepad
+        padP1.on('up',(index,value,button) =>{
             if(index === 9){
                 this.scene.wake('PlayScene');
                 this.scene.remove('PauseScene');
             }
         });
 
-        this.input.gamepad.pad3.on('up',(index,value,button) =>{
+        padP2.on('up',(index,value,button) =>{
             if(index === 9){
                 this.scene.wake('PlayScene');
                 this.scene.remove('PauseScene');
             }
         });
+
+        
     }
 }
